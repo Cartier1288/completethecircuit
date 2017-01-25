9deefebd30894995bb3dda0213ecbe4f69569491
cartier1288 <cartier1288@gmail.com>
2017-01-25 14:52:30 -0500

    Comments

--- src/Board.java
+++ src/Board.java
@@ -11,114 +11,115 @@ public class Board {
 		/* 
 		Layout of board format:
 		
-		 -Board Name-
-		 +Board description+
+		 title=Board Name;
+		 description=Board description;
 		 
-		 | (3) (1) (-1) (1) (4) ; (New line)
-		   (2) (-1) (-1) (-1) (2) ;
-		   (5) (1) (6, 0) (1) (6) | (End file)
+		 / (3) (1) (-1) (1) (4)
+		   (2) (-1) (-1) (-1) (2) 
+		   (5) (1) (8, 1, 10) (1) (6) / ;Default board
 		
-		 | [0, 2](5, 0, 10) |
-		 
+		 / {[0, 2](11, -1, 3)} / ;Corrections
 		*/
 		
-		for(int i = 0; i < boardComponents.length; i++)
-			for(int j = 0; j < boardComponents[0].length; j++) {
-				if(i % 2 == 0)
-					boardComponents[i][j] = new ComponentIndex(0);
-				else
-					boardComponents[i][j] = new ComponentIndex(8, i + (j * 5), (i * 10) + (j * 100));
-				boardChanges[i][j] = new ComponentIndex(-1);
-			}
-		
 		try {
-			Scanner boardScanner = new Scanner(new File(boardPath));
+			Scanner boardScanner = new Scanner(new File(boardPath)); //Create a Scanner to iterate through the board file
+			/*
+			 * Set the delimiters for the scanner; title= (for the title), 
+			 * description= (for the description), ; (for the end of the 
+			 * information), and / (for the components).
+			 */
 			boardScanner.useDelimiter("title=|description=|;|\\/");
 			
-			String title = boardScanner.next();
-			if(title.equals("")) System.out.println("Improper formatting for board: " + boardPath);
+			String title = boardScanner.next(); //Get the title
+			if(title.equals("")) System.out.println("Improper formatting for board: " + boardPath); //If the title is empty, the file is improperly formatted.
 			this.boardName = title;
 			
-			boardScanner.next();
-			String description = boardScanner.next();
-			if(description.equals("")) System.out.println("Improper formatting for board: " + boardPath);
+			boardScanner.next(); //Go to the next section
+			String description = boardScanner.next(); //Get the description
+			if(description.equals("")) System.out.println("Improper formatting for board: " + boardPath); //If the description is empty, the file is improperly formatted.
 			this.boardDescription = description;
 			
 			
-			boardScanner.next();
-			String defaultBoard = boardScanner.next();
+			boardScanner.next(); //Go to the next section
+			String defaultBoard = boardScanner.next(); ///Get the default components
 			
+			//Make a matcher to extract the component information, for one line.
 			Matcher widthMatcher = Pattern.compile("\\((.*?)\\)").matcher(defaultBoard.substring(0, defaultBoard.indexOf('\n')));
-			int columns = 0;
+			int columns = 0; //Set the columns to a default of 0
 			
-			while(widthMatcher.find()) columns++; 
+			while(widthMatcher.find()) columns++; //For every extracted string increment the columns
 			
-			Matcher defaultMatcher = Pattern.compile("\\((.*?)\\)").matcher(defaultBoard);
+			Matcher defaultMatcher = Pattern.compile("\\((.*?)\\)").matcher(defaultBoard); //Make a matcher to extract all component information
 			
+			//Get the total number of components
 			int totalLength = 0;
 			while(defaultMatcher.find()) totalLength++;
 			
-			int rows = totalLength / columns;
+			int rows = totalLength / columns; //Calculate the number of rows using the total number of components divided by columns
 			
-			boardComponents = new ComponentIndex[rows][columns];
+			boardComponents = new ComponentIndex[rows][columns]; //Initialize the boardComponents (default) with dimensions rows x columns
 			
-			defaultMatcher.reset();
+			defaultMatcher.reset(); //Reset the matcher, in order to go through the information
 			int inc = 0;
-			while(defaultMatcher.find()) {
-				String[] component = defaultMatcher.group(1).replaceAll(" ", "").split(",");
-				int index = Integer.parseInt(component[0]);
+			while(defaultMatcher.find()) { //Iterate through the component information
+				String[] component = defaultMatcher.group(1).replaceAll(" ", "").split(","); //Split the component into index, count, and value.
+				int index = Integer.parseInt(component[0]); //Parse the index
 				int count = -1;
 				double value = -1;
 				
-				if(component.length > 1)
+				if(component.length > 1) //If there is enough information, calculate the count.
 					count = Integer.parseInt(component[1]);
-				if(component.length > 2)
+				if(component.length > 2) //If there is enough information, calculate the value (double).
 					value = Double.parseDouble(component[2]);
 				
-				boardComponents[inc / columns][inc % columns] = new ComponentIndex(index, count, value);
+				boardComponents[inc / columns][inc % columns] = new ComponentIndex(index, count, value); //Calculate the coordinates of the component, and fill it with an instantiated ComponentIndex.
 				
-				inc++;
+				inc++; //Increment the index
 			}
 			
 			
+			//Get the correct components 
 			boardScanner.next();
 			String correctBoard = boardScanner.next();
 			
+			//Initialize boardChanges with empty space
 			boardChanges = new ComponentIndex[rows][columns];
 			for(int i = 0; i < rows; i++)
 				for(int j = 0; j < columns; j++)
 					boardChanges[i][j] = new ComponentIndex(-1);
 			
+			//Create a Matcher to extract the board changes
 			Matcher changeMatcher = Pattern.compile("\\{(.*?)\\}").matcher(correctBoard);
 			
+			//Iterate through the changes
 			while(changeMatcher.find()) {
 				String match = changeMatcher.group(1).replaceAll(" ", "");
 				int middle = match.indexOf(']');
-				String componentCoordinates = match.substring(1, middle);
-				String componentInfo = match.substring(middle + 2, match.length() - 1);
+				String componentCoordinates = match.substring(1, middle); //Extract the component coordinates
+				String componentInfo = match.substring(middle + 2, match.length() - 1); //Extract the component information
 				
-				String[] coordinates = componentCoordinates.split(",");
+				String[] coordinates = componentCoordinates.split(","); //Separate the coordinates
 				int x = Integer.parseInt(coordinates[0]),
 					y = Integer.parseInt(coordinates[1]);
 				
-				String[] component = componentInfo.split(",");
-				int index = Integer.parseInt(component[0]);
+				String[] component = componentInfo.split(","); //Split the component information
+				int index = Integer.parseInt(component[0]); //Parse the component index
 				int count = -1;
 				double value = -1.0;
 				
-				if(component.length > 1)
+				if(component.length > 1) //If there is enough information, parse the count.
 					count = Integer.parseInt(component[1]);
-				if(component.length > 2)
+				if(component.length > 2) //If there is enough information, parse the value.
 					value = Double.parseDouble(component[2]);
 				
-				boardChanges[y][x] = new ComponentIndex(index, count, value);
+				boardChanges[y][x] = new ComponentIndex(index, count, value); //Create a new ComponentIndex, using the gathered information, in its respective coordinate.
 				
-				numberOfChanges++;
+				numberOfChanges++; //Increment the number of changes, for external use (to test when the board is completed).
 			}
 			
-			boardScanner.close();
+			boardScanner.close(); //Close the scanner
 			
-			
+			//Check if there is a progress file for the board, if so, the board has been completed, so scan it to extract the number of misplaced components.
 			File progressFile = new File("res/progress/" + boardName.replaceAll(" ", "").toLowerCase() + "-progress.pgs");
 			if(progressFile.exists()) {
 				Scanner progressScanner = new Scanner(progressFile);
@@ -133,34 +134,42 @@ public class Board {
 		}
 	}
 	
+	//Function to get the name of the board
 	public String getName() {
 		return boardName;
 	}
 	
+	//Function to get the description of the board
 	public String getDescription() {
 		return boardDescription;
 	}
 	
+	//Function to get whether the board has been completed
 	public boolean getCompletion() {
 		return completion;
 	}
 	
+	//Method to set the board as complete
 	public void setComplete() {
 		completion = true;
 	}
 	
+	//Function to get the number of misplaced components
 	public int getMisplaced() {
 		return misplaced;
 	}
 	
+	//Function to set the number of misplaced components
 	public void setMisplaced(int misplaced) {
 		this.misplaced = misplaced;
 	}
-	
+
+	//Function to get the number of changed components
 	public int getNumberOfChanges() {
 		return numberOfChanges;
 	}
 	
+	//Function to test whether a ComponentIndex, at x by y, outlines the correct component.
 	public boolean match(ComponentIndex component, int x, int y) {
 		ComponentIndex correctComponent = boardChanges[y][x];
 		System.out.println("Index: " + correctComponent.index + " to " + component.index + ", " + correctComponent.count + " to " + component.count + ", " + correctComponent.value + " to " + component.value);
@@ -169,11 +178,13 @@ public class Board {
 			 && correctComponent.value == component.value);
 	}
 	
+	//Function to get the default board
 	public ComponentIndex[][] getDefaultBoardComponents() {
 		return boardComponents;
 	}
 	
 	
+	//
 	private String boardName = "This is a title";
 	private String boardDescription = "This is a description";
 	
--- src/CompleteTheCircuit.java
+++ src/CompleteTheCircuit.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
@@ -18,48 +19,48 @@ public class CompleteTheCircuit {
 	public static JFrame frame = new JFrame("Complete the Circuit - Main Menu");
 	
 	public static void main(String[] args) {
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1280, 720);
-		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Set the default close operation for the frame to JFrame.EXIT_ON_CLOSE, to terminate the program, when the frame is closed.
+		frame.setSize(1280, 720); //Set the frame's dimensions to 720p
+		frame.setLocationRelativeTo(null); //Center the frame
 		
-		Component.init();
+		Component.init(); //Load the default components
 		
-		JPanel mainMenu = new JPanel();
-		mainMenu.setLayout(new GridLayout(2, 1));
+		JPanel mainMenu = new JPanel(); //Instantiate the panel for the mainMenu
+		mainMenu.setLayout(new GridLayout(2, 1)); //Set the layout for the mainMenu to a new GridLayout with dimensions 1x2
 		
-		BoardList boardList = new BoardList(frame, mainMenu);
+		BoardList boardList = new BoardList(frame, mainMenu); //Instantiate a new BoardList, to hold the BoardButtons
 		
 		JPanel titlePanel = new JPanel();
 		titlePanel.setLayout(new GridLayout(1, 1));
 		
 		JLabel title = new JLabel("Complete the Circuit", JLabel.CENTER);
-		title.setFont(new CustomFont("/fonts/adamcg.ttf", Font.BOLD, 80).getFont());
+		title.setFont(new CustomFont("/fonts/adamcg.ttf", Font.BOLD, 80).getFont()); //Set the title font to a new CustomFont, with Adam.CG PRO, and size 80px.
 		
-		titlePanel.add(title);
+		titlePanel.add(title); //Add the title to the panel
 		
 		
-		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setLayout(new GridLayout(2, 1, 0, 50));
-		buttonsPanel.setBorder(new EmptyBorder(0, 300, 100, 300));
+		JPanel buttonsPanel = new JPanel(); //Create a panel to hold the main menu button 
+		buttonsPanel.setLayout(new GridLayout(2, 1, 0, 50)); //Set the GridLayout to dimensions 1x2 with a vertical gap of 50px
+		buttonsPanel.setBorder(new EmptyBorder(0, 300, 100, 300)); //Space the panel from the title, and the bottom of the frame.
 		
-		CustomFont buttonFont = new CustomFont("/fonts/bneueregular.ttf", Font.BOLD, 42);
+		CustomFont buttonFont = new CustomFont("/fonts/bneuebold.ttf", Font.PLAIN, 42); //Create a new CustomFont for the buttons, with Bebas Neue Bold, and size 42px.
 		Color buttonColor = new Color(46, 52, 68);
 		LineBorder buttonBorder = new LineBorder(new Color(32, 38, 48), 3);
 		
-		JButton boardListButton = new JButton("Board List");
-		boardListButton.addActionListener(new SwitchPanel(frame, boardList, "Complete the Circuit - Board List"));
+		JButton boardListButton = new JButton("Board List"); //Create the button to open the BoardList
+		boardListButton.addActionListener(new SwitchPanel(frame, boardList, "Complete the Circuit - Board List")); //Add a new SwitchPanel as the button's ActionListener, to switch from the main menu to the BoardList
 		boardListButton.setFont(buttonFont.getFont());
 		boardListButton.setBackground(buttonColor);
 		boardListButton.setForeground(Color.white);
 		boardListButton.setBorder(buttonBorder);
 		
-		JButton helpDocumentationButton = new JButton("Help and Documentation");
-		helpDocumentationButton.addActionListener(new ActionListener() {
+		JButton helpDocumentationButton = new JButton("Help and Documentation"); //Create a button to open the program documentation
+		helpDocumentationButton.addActionListener(new ActionListener() { //Add an ActionListener to the button
 			public void actionPerformed(ActionEvent e) {
 				try {
-					Desktop.getDesktop().browse(new File("res/documentation/index.html").toURI());
+					Desktop.getDesktop().browse(new File("res/documentation/index.html").toURI()); //Use the Desktop functionality to open a browser with the documentation
 				} catch (IOException e1) {
-					e1.printStackTrace();
+					JOptionPane.showMessageDialog(null, "Unable to open documentation");
 				}
 			}
 		});
@@ -68,13 +69,15 @@ public class CompleteTheCircuit {
 		helpDocumentationButton.setForeground(Color.white);
 		helpDocumentationButton.setBorder(buttonBorder);
 		
+		//Add the buttons to the buttonsPanel
 		buttonsPanel.add(boardListButton);
 		buttonsPanel.add(helpDocumentationButton);
 		
+		//Add the title and buttons to the main menu
 		mainMenu.add(titlePanel);
 		mainMenu.add(buttonsPanel);
 		
-		frame.setContentPane(mainMenu);
-		frame.setVisible(true);
+		frame.setContentPane(mainMenu); //Set the content-pane of the frame to the main menu
+		frame.setVisible(true); //Make the frame visible to the user
 	}
 }
